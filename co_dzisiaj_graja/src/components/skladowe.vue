65f21f557434c3cd0f5fa8b20b6abe0402d54a62
Gravoos <kibic555@gmail.com>
2020-05-03 18:42:20 +0200

    Add skladowe

--- co_dzisiaj_graja/src/components/skladowe.vue
+++ co_dzisiaj_graja/src/components/skladowe.vue
@@ -1,13 +1,9 @@
 <template>
   <div  class="skladowe">
-    <h3 id="opis"><a href="https://github.com/Gravoos/Gravoos
-    .github.io/tree/master/co_dzisiaj_graja">Cała aplikacja</h3>
-    <h3 id="opis"><a href="https://github.com/Gravoos/Gravoos
-    .github.io/tree/master/co_dzisiaj_graja/src/components">Komponenty aplikacji</h3>
-    <h3 id="opis"><a href="https://github.com/Gravoos/Gravoos
-    .github.io/tree/master/co_dzisiaj_graja/src/assets">Obrazki</h3>
-    <h3 id="opis"><a href="https://github.com/Gravoos/Gravoos
-    .github.io/tree/master/co_dzisiaj_graja/src/router">Wszysktkie ścieżkki</h3>
+    <h3 id="opis"><a href="https://github.com/Gravoos/Gravoos.github.io/tree/master/co_dzisiaj_grajat">Cała aplikacja</a></h3>
+    <h3 id="opis"><a href="https://github.com/Gravoos/Gravoos.github.io/tree/master/co_dzisiaj_graja/src/components">Komponenty aplikacji</a></h3>
+    <h3 id="opis"><a href="https://github.com/Gravoos/Gravoos.github.io/tree/master/co_dzisiaj_graja/src/assets">Obrazki</a></h3>
+    <h3 id="opis"><a href="https://github.com/Gravoos/Gravoos.github.io/tree/master/co_dzisiaj_graja/src/router">Wszysktkie ścieżkki</a></h3>
   </div>
 </template>
 
