9261f620db817faca6f1c2dee6de68ddb4c23312
Gravoos <kibic555@gmail.com>
2020-05-03 18:36:20 +0200

    Add skladowe

--- co_dzisiaj_graja/src/components/skladowe.vue
+++ co_dzisiaj_graja/src/components/skladowe.vue
@@ -1,6 +1,13 @@
 <template>
   <div  class="skladowe">
-<h3 id="opis">1 składowa</h3>
+    <h3 id="opis"><a href="https://github.com/Gravoos/Gravoos
+    .github.io/tree/master/co_dzisiaj_graja">Cała aplikacja</h3>
+    <h3 id="opis"><a href="https://github.com/Gravoos/Gravoos
+    .github.io/tree/master/co_dzisiaj_graja/src/components">Komponenty aplikacji</h3>
+    <h3 id="opis"><a href="https://github.com/Gravoos/Gravoos
+    .github.io/tree/master/co_dzisiaj_graja/src/assets">Obrazki</h3>
+    <h3 id="opis"><a href="https://github.com/Gravoos/Gravoos
+    .github.io/tree/master/co_dzisiaj_graja/src/router">Wszysktkie ścieżkki</h3>
   </div>
 </template>
 
